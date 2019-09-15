49475eeb9a381a410b0aeb092e81461618b288d5
yunyuncc <wyy584322202@outlook.com>
2019-09-15 23:23:12 +0800

    change project tree

--- build.py
+++ build.py
@@ -3,20 +3,22 @@ import subprocess
 import os
 def run(cmd):
     res = subprocess.check_call(cmd, shell=True)
+build_dir = "malice/build"
+base_dir = os.getcwd()
+if not os.path.exists(build_dir):
+    os.mkdir(build_dir)
 
-if not os.path.exists("build"):
-    os.mkdir("build")
-
-os.chdir("build")
-run("cmake ..")
+os.chdir(build_dir)
+full_build_dir=os.getcwd()
+run("cmake " + base_dir)
 run("make")
 run("make test")
 run("rm -rf gcov_out")
 run("rm -f coverage.info")
-run("lcov -b . --capture --directory .  --output-file coverage.info")
-run("genhtml coverage.info --output-directory gcov_out")
-pwd = os.getcwd()
-html_path = pwd + "/gcov_out"
+os.chdir(base_dir)
+run("lcov -b ./malice --no-external  --capture --directory ./malice --output-file malice/build/coverage.info")
+run("genhtml malice/build/coverage.info --output-directory malice/build/gcov_out")
+html_path = full_build_dir + "/gcov_out"
 print(html_path)
 run("rm -f /www")
 cmd = "ln -s " + html_path + " /www"
